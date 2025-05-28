064c6a14a6ed9434b68ca1b9d3f9bf83ce93c944
PeanutKoa <dtrkicks@gmail.com>
2025-05-27 21:16:10 -0700

    evil

--- init.el
+++ init.el
@@ -22,7 +22,8 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (column-number-mode)
-(setq display-line-numbers 'relative)
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
 (dolist (mode '(org-mode-hook
                 term-mode-hook
                 shell-mode-hook
@@ -32,4 +33,30 @@
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (use-package catppuccin-theme :straight t)
 (load-theme 'catppuccin :no-confirm)
-(use-package rainbow-delimiters :straight t)
+(use-package rainbow-delimiters
+  :straight t
+  :hook (prog-mode . (rainbow-delimiters-mode))
+
+  
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
