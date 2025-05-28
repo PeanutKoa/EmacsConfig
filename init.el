a987e5aa06dd12e018465e4f38fe10ccf85f63a9
PeanutKoa <dtrkicks@gmail.com>
2025-05-27 20:44:22 -0700

    fuck github, heres some boilerplate shit

--- init.el
+++ init.el
@@ -14,3 +14,12 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 (straight-use-package 'use-package)
+
+;; Setting all of the standard shit, such as fonts, theme, and removing scroll bar
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 120)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(use-package catppuccin-theme :straight t)
