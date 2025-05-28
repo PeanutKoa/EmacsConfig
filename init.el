f03f734d348c89fb356deb3501931825e83856f0
PeanutKoa <dtrkicks@gmail.com>
2025-05-27 21:03:42 -0700

    more boilerplate, still no evil yet

--- init.el
+++ init.el
@@ -17,10 +17,19 @@
 
 ;; Setting all of the standard shit, such as fonts, theme, and removing scroll bar
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 120)
+(setq inhibit-startup-message t)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (column-number-mode)
-(global-display-line-numbers-mode t)
+(setq display-line-numbers 'relative)
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (use-package catppuccin-theme :straight t)
 (load-theme 'catppuccin :no-confirm)
+(use-package rainbow-delimiters :straight t)
